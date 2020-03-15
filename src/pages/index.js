2064b72a2f823ba26ff3db370f5c0d19410cbc26
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-03-15 17:27:28 +0100

    commited unsaved file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ShowcaseSection from "../components/ShowcaseSection"
+import ShowcaseSection from "../components/showcaseSection"
 import { graphql } from 'gatsby'
 
 const IndexPage = props => {
