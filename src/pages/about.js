e6a960ffdfe94ec6f618e23c59a01667c36b114a
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-03-24 19:21:28 +0100

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ const H1 = styled.h1`
 `
 
 const About = styled.p`
-    width: 50%;
+    width: 70%;
 
     @media (max-width: 500px) {
         width: 100%;
@@ -38,14 +38,31 @@ export default props => {
             <Content>
                 <H1>About</H1>
                 <About>
-                    I'm a Brussels based web developer and technology enthousiast with a passion for generative art.
+                    I'm a Brussels based programmer and technology enthousiast with a passion for generative art. 
+                    I became acquainted with Processing through my studies and quickly became interested in the concept of
+                    generating something visually attractive and unique each time a program is run.
+                    
+                    <br/><br/>
+                    More recently,
+                    I discovered the vibrant community of pen plotters. It's a lot of fun
+                    to be able to draw your artwork using fountain pens, markers or even paint brushes using scalable
+                    vector graphics instead of having to rely on high resolution images and professional print shops.
+                    Because I often struggle with having too much options it's also very welcoming to being limited to generating
+                    lines only.
+
+                    <br/><br/>
+                    Currently I make most of my sketches using javascript as it is easier for me to develop and debug.
+                    Starting out using P5js, I eventually switched to Paperjs for generating SVG files since I never 
+                    quite got around getting the SVG export function in P5js to work with webpack.
+                    I only ever switch back to Processing when more performance is needed.
+
                     <br/><br/>
                     This site is mainly used for posting more information on the process of making
                     my sketches. Most of them are available to test out in the browser although it must
                     be said they are pretty messy and I made few attempts to make them 
                     performant or easy to use so beware, they might freeze your browser. For fellow programmers
                     the (also sloppy at times) source code for sketches and this website is also made 
-                    publicly available on Github.
+                    publicly available on my Github.
                 </About>
             </Content>
         </Layout>
