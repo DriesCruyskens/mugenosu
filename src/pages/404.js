ab5873869234158bc7d5b915a014e62b413ac901
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-08-03 18:45:11 +0200

    Removed google analytics and leftover font-family on 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -17,7 +17,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <StyledSection>
-      <h1 style={{fontFamily: "ABeeZee"}}>NOT FOUND</h1>
+      <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </StyledSection>
   </Layout>
